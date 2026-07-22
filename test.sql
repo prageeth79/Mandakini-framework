5c1643fe5dca1740e8cc4ab6974ad3c0901151ea
Prageeth Niranjan <prageeth79@gmail.com>
2026-07-22 12:57:45 +0530

    sql scripts change

--- test.sql
+++ test.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 26, 2026 at 02:33 PM
+-- Generation Time: Jul 22, 2026 at 09:23 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
