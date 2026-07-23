8ed024ed74ec57ca6bb307ba57db5f506412a8ad
Prageeth Niranjan <prageeth79@gmail.com>
2026-07-23 08:27:06 +0530

    remove reffernc

--- views/contact.php
+++ views/contact.php
@@ -15,23 +15,3 @@ echo $form->field($model, 'body')->textareaField();
 echo '<button type="submit" class="btn btn-primary">Send Message</button>';
 $form->end();
 ?>
-<!---
-<form class="row g-3" action="" method="post" >
-  <div class="col-md-6">
-    <label for="inputEmail4" class="form-label">Email</label>
-    <input type="email" name="email" class="form-control" id="inputEmail4">
-  </div>
-  <div class="col-md-6">
-    <label for="inputSubject4" class="form-label">Subject</label>
-    <input name="subject" type="text" class="form-control" id="inputSubject4">
-  </div>
-  <div class="col-12">
-    <label for="inputBody" class="form-label">Message</label>
-    <textarea name="body" class="form-control" id="inputBody" placeholder="Your message here..."></textarea>
-  </div>
-  
-  <div class="col-12">
-    <button type="submit" class="btn btn-primary">Send Message</button>
-  </div>
-</form>
---->
