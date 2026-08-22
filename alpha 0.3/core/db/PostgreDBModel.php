3bc2e7b52b8645f8d40722f218024ed2888359df
Prageeth Niranjan <prageeth79@gmail.com>
2026-08-22 16:59:31 +0530

    new versions

--- alpha 0.3/core/db/PostgreDBModel.php
+++ alpha 0.3/core/db/PostgreDBModel.php
@@ -8,7 +8,7 @@ use app\core\Application;
  * and provides sensible defaults for `attributes()` and `primaryKey()`.
  */
 
-abstract class PostgresDBModel extends DBModel
+abstract class PostgreDBModel extends DBModel
 {
     /**
      * Cache of discovered columns metadata per table.
